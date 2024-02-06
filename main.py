57c158b7677a7428afb300ce9285ffdf30552695
Storm-Visser <storm.visser@student.nhlstenden.com>
2024-02-06 13:56:04 +0100

    Added Selection

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ EvoSystem.StartSim(
     Seed = 0,
     UseLinReg = 0, # True for LinReg false for Sine
     UseCrowding = 0,
-    BitstringLength = 0,
+    BitstringLength = 15,
     MutationRate = 0.0,
     CrossoverRate = 0.0,
-
-
+    PopSize = 100,
+    AmountOfParents = 10
 )

1042ecfb7e980c1dd9c2863ea8cab98888fa2410
Storm-Visser <storm.visser@student.nhlstenden.com>
2024-02-06 14:57:26 +0100

    implemented crossover

--- EvoSystem.py
+++ EvoSystem.py
@@ -6,9 +6,10 @@ from LinReg import LinReg
 
 def StartSim(AmountOfGen, Seed, UseLinReg, UseCrowding, BitstringLength, MutationRate, CrossoverRate, PopSize, AmountOfParents):
     Pop = InitPop(BitstringLength, PopSize)
+    # print(Pop)
     Results = []
     regressor = LinReg()
-    data = pd.read_csv("dataset.txt", header=None)
+    data = pd.read_csv("Data/Data.txt", header=None)
 
     for _ in range(AmountOfGen):
         #get the top X amount of parents
@@ -26,7 +27,7 @@ def StartSim(AmountOfGen, Seed, UseLinReg, UseCrowding, BitstringLength, Mutatio
             Match(ChildrenM)
             Compete(ChildrenM)
             
-        Results.append(SaveResults())
+        Results.append(SaveResults(Pop))
     return
 
 
@@ -48,9 +49,9 @@ def Selection(Pop, UseLinReg, AmountOfParents):
     Selected = dict()
     for Individual in Pop:
         if UseLinReg:
-            Fitness = FitnesFunc1(Individual)
+            Fitness = fitnesML(Individual)
         else: 
-            Fitness = FitnesFunc2(Individual)
+            Fitness = fitnesIntValue(Individual)
         # Add the individual and its value to the dict
         Selected[Individual] = Fitness
     # Sort the dictionary to its values
@@ -59,13 +60,37 @@ def Selection(Pop, UseLinReg, AmountOfParents):
     Parents = [item[0] for item in SelectedSorted[:AmountOfParents]]
     return Parents, SelectedSorted
 
-    
-
-    return[]
-
 def Crossover(Parents, Rate):
+    Children = []
+    checkedParents = []
+    for Parent in Parents:
+        # break when 10 children are found
+        if len(Children) >= len(Parents): break
+        # dont add parents that were already used
+        if Parent in checkedParents:
+            continue
+        # update the used parents
+        checkedParents.append(Parent)
+        # Randomly select another parent that hassnt been used for crossover
+        OtherParent = random.choice([x for x in Parents if x not in checkedParents])
+        # update the used parent
+        checkedParents.append(OtherParent)
+        # Check if crossover should occur based on the rate
+        if random.random() < Rate:
+            # Randomly select a crossover point
+            crossover_point = random.randint(1, len(Parents) - 1)
 
-    return[]
+            # Perform crossover to create two children
+            Child1 = Parent[:crossover_point] + OtherParent[crossover_point:]
+            Child2 = OtherParent[:crossover_point] + Parent[crossover_point:]
+        else:
+            # If no crossover, the children are copies of the parents
+            Child1 = Parent
+            Child2 = OtherParent
+
+        Children.extend([Child1, Child2])
+
+    return Children
 
 def Mutate(Children, Rate):
     return[]
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import EvoSystem
 
 EvoSystem.StartSim(
-    AmountOfGen = 0,
+    AmountOfGen = 1,
     Seed = 0,
     UseLinReg = 0, # True for LinReg false for Sine
     UseCrowding = 0,
-    BitstringLength = 15,
-    MutationRate = 0.0,
-    CrossoverRate = 0.0,
+    BitstringLength = 5,
+    MutationRate = 0.01,
+    CrossoverRate = 0.7,
     PopSize = 100,
     AmountOfParents = 10
 )

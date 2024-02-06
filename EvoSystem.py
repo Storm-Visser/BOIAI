56a85899929b068fbadc3a8f3c38ba16525d7acb
williamvoster <william.voster@carnain.com>
2024-02-06 13:27:58 +0100

    2 fitness functions

--- EvoSystem.py
+++ EvoSystem.py
@@ -1,9 +1,15 @@
+import numpy as np
+import pandas as pd
 
+from LinReg import LinReg
 
 
 def StartSim(AmountOfGen, Seed, UseLinReg, UseCrowding, BitstringLength, MutationRate, CrossoverRate):
     Pop = InitPop(BitstringLength)
     Results = []
+    regressor = LinReg()
+    data = pd.read_csv("dataset.txt", header=None)
+
     for _ in range(AmountOfGen):
         # where do we select fitness funtion?
         Selection(Pop)
@@ -37,7 +43,10 @@ def Compete(Pop):
 def SaveResults(Pop):
     return[]
 
+def fitnesML(regressor: LinReg, data:pd.DataFrame, bitstring:str):
+    X = regressor.get_columns(data.values, bitstring)
+    return regressor.get_fitness(X[:,:-1], X[:,-1])
 
+def fitnesIntValue(bitstring):
+    return int(bitstring, 2)
 
-
-    ## mycomment
